32a45155b42e32ab18c907c33fdddef3a8587b39
joaomiol <joao-miguel.oliveira-fernandes@capgemini.com>
2022-12-21 16:51:37 +0000

    Add connection to currency service

---
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -0,0 +1,70 @@
+package com.example.bookservice.controller;
+
+import com.example.bookservice.model.Book;
+import com.example.bookservice.proxy.CurrencyProxy;
+import com.example.bookservice.repository.BookRepository;
+import com.example.bookservice.response.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private BookRepository repository;
+    @Autowired
+    private CurrencyProxy proxy;
+
+    /*@GetMapping(value = "/{id}/{currency}")
+    public Book findBook(@PathVariable("id") Long id,
+                         @PathVariable("currency") String currency){
+
+        Book book = repository.getById(id);
+        if(book == null) throw new RuntimeException("Book not found!");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+        var response = new RestTemplate().getForEntity("http://localhost:8000/currency-service/{amount}/{from}/{to}",
+                Currency.class,
+                params);
+
+        var exchange = response.getBody();
+
+        String port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
+        return book;
+
+    }*/
+    @GetMapping(value = "/{id}/{currency}")
+    public Book findBook(@PathVariable("id") Long id,
+                         @PathVariable("currency") String currency){
+
+        Book book = repository.getById(id);
+        if(book == null) throw new RuntimeException("Book not found!");
+
+
+        var exchange = proxy.getCurrency(book.getPrice(), "USD", currency);
+
+        String port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
+        return book;
+
+    }
+
+}
---
+++ src/main/java/com/example/bookservice/proxy/CurrencyProxy.java
@@ -0,0 +1,18 @@
+package com.example.bookservice.proxy;
+
+import com.example.bookservice.response.Currency;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "currency-service", url = "localhost:8000")
+public interface CurrencyProxy {
+
+    @GetMapping(value = "/currency-service/{amount}/{from}/{to}")
+    public Currency getCurrency(
+            @PathVariable("amount") Double amount,
+            @PathVariable("from")String from,
+            @PathVariable("to")String to);
+}
