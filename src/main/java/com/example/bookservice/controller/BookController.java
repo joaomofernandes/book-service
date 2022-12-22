d75ef3ecd8b9f95652243881d5d2eec18622aaf8
joaomiol <joao-miguel.oliveira-fernandes@capgemini.com>
2022-12-22 17:40:12 +0000

    add resilience4j

--- src/main/java/com/example/bookservice/controller/BookController.java
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -10,11 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("book-service")
